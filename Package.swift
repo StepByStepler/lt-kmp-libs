2867fa545e48de1ad1d761b185d14b3c4e40c4ae
StepByStepler <42212904+StepByStepler@users.noreply.github.com>
2024-10-20 18:01:50 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
    targets: [
       .binaryTarget(
          name: "LevelTravelChat",
-         url: "https://github.com/StepByStepler/lt-kmp-libs/releases/download/v1.0.3/LevelTravelChat.xcframework.zip",
-         checksum: "0a10b61a858551c6b0aa036cbb7007dd558510e42b8799f9c793423468fae8c3")
+         url: "https://github.com/StepByStepler/lt-kmp-libs/releases/download/v1.0.4/LevelTravelChat.xcframework.zip",
+         checksum: "1d5875ed1a2463308cd34fa8b23477b36600258aa60a2d9e6178799b3aaeb7d8")
    ]
 )
